efe4f07877819ae76db0fe1891dd6d45eed56b2b
Rafal Kobak <raf.kobak@gmail.com>
2018-09-17 21:15:43 +0000

    First working version

--- terrariumController.py
+++ terrariumController.py
@@ -5,6 +5,7 @@ sys.path.append("./subrepos/Adafruit_Python_DHT")
 import Adafruit_DHT
 import requests
 from gpiozero import Energenie
+from time import sleep
 
 def getHumidityAndTemperature():
     sensorModel = Adafruit_DHT.DHT22
@@ -12,21 +13,51 @@ def getHumidityAndTemperature():
     secondSensorGpio = 26
 
     firstSensorHumidity, firstSensorTemperature = Adafruit_DHT.read_retry(sensorModel, firstSensorGpio)
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(firstSensorTemperature, firstSensorHumidity))
+    print('Read out #1: Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(firstSensorTemperature, firstSensorHumidity))
 
     secondSensorHumidity, secondSensorTemperature = Adafruit_DHT.read_retry(sensorModel, secondSensorGpio)
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(secondSensorTemperature, secondSensorHumidity))
+    print('Read out #2: Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(secondSensorTemperature, secondSensorHumidity))
 
-    humidity = (firstSensorHumidity + secondSensorHumidity) / 2
-    temperature = (firstSensorTemperature + secondSensorTemperature) / 2
+    humidityAvg = (firstSensorHumidity + secondSensorHumidity) / 2
+    temperatureAvg = (firstSensorTemperature + secondSensorTemperature) / 2
 
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-    return {humidity, temperature}
+    print('Read out #AVG Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperatureAvg, humidityAvg))
+    return (humidityAvg, temperatureAvg)
 
-hum, temp = getHumidityAndTemperature()
-thingSpeakIoTKey = "8N9BL598BIFY4LV7"
-requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingSpeakIoTKey, 'field1':temp, 'field2':hum})
+enableHeaterTemperature_Celsius = 25
+disableHeaterTemperature_Celsius = 26
 
-Energenie(3, initial_value=False)
+enableHumidifierMoistness_Procent = 94
+disableHumidifierMoistness_Procent = 97
 
-print ("Bla")
+heater = Energenie(2, initial_value=False) 
+humidifier = Energenie(3, initial_value=False)
+
+while True:
+    lastReadOn = getHumidityAndTemperature() 
+    humidity = lastReadOn[0]
+    temperature = lastReadOn[1]
+
+    thingSpeakIoTKey = "8N9BL598BIFY4LV7"
+    requests.post('https://api.thingspeak.com/update.json', data = {'api_key':thingSpeakIoTKey, 'field1':temperature, 'field2':humidity})
+
+    if int(humidity) < enableHumidifierMoistness_Procent and not humidifier.is_active:
+        humidifier.on()
+        print('Humidifier enabled!')
+
+    if int(humidity) > disableHumidifierMoistness_Procent and humidifier.is_active:
+        humidifier.off()
+        print('Humidifier disabled!')
+        
+
+    if temperature < enableHeaterTemperature_Celsius and not heater.is_active:
+        heater.on()
+        print('Heater enabled!')
+
+    if temperature > disableHeaterTemperature_Celsius and heater.is_active:
+        heater.off()
+        print('Heater disabled!')
+
+    sleep(10.0)
+
+print ("Execution terminated!")
